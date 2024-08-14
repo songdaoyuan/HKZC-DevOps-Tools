f223d9ed2b4a3a3b012e449cca287e4359b4267a
宋导 <835128023@qq.com>
2024-08-14 11:03:43 +0800

    update InstallDocker.sh

--- shell/InstallDocker.sh
+++ shell/InstallDocker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# code by songdaoyuan@20240814 
+# code by songdaoyuan@20240814
 # 检测OS版本 -> 卸载现有的Docker -> 安装生产使用的20.10.21
+# 流程参考自:https://mirrors.tuna.tsinghua.edu.cn/help/docker-ce/
+# 所有需要通过*.docker.*下载的文件均已备份在gitee或者使用TUNA源
 
 set -exu
 
@@ -21,12 +23,10 @@ case "$os_name" in
         docker-latest-logrotate \
         docker-logrotate \
         docker-engine
-    yum install -y yum-utils
-    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    sed -i 's+https://download.docker.com+https://mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
     ;;
 "Ubuntu" | "Debian")
-    apt-get install example-package
+    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt-get remove $pkg; done
+    apt-get update
     ;;
 *)
     echo "Unsupported distribution: $os_name"
@@ -34,33 +34,32 @@ case "$os_name" in
     ;;
 esac
 
-# For Debian/Ubuntu/Raspbian Distribute ************************************
-# for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt-get remove $pkg; done
-# apt-get update
-# apt-get install ca-certificates curl gnupg
-# install -m 0755 -d /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-# sudo chmod a+r /etc/apt/keyrings/docker.gpg
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
-#   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-#   tee /etc/apt/sources.list.d/docker.list > /dev/null
-# Debian/Ubuntu/Raspbian End ************************************
-
-# For CentOS/RHEL Distribute ********************************
-# yum remove docker \
-#                   docker-client \
-#                   docker-client-latest \
-#                   docker-common \
-#                   docker-latest \
-#                   docker-latest-logrotate \
-#                   docker-logrotate \
-#                   docker-engine
-# yum install -y yum-utils
-# yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-# sed -i 's+https://download.docker.com+https://mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
-# CentOS/RHEL End ************************************
-
 export DOWNLOAD_URL="https://mirrors.tuna.tsinghua.edu.cn/docker-ce"
 
-wget -O- https://gitee.com/songdaoyuan/hkzc-dev-ops-tools/raw/master/shell/GetDocker.sh | sh --version 20.10.21
+wget -O- https://gitee.com/songdaoyuan/hkzc-dev-ops-tools/raw/master/Docker/GetDocker.sh | sh --version 20.10.21
+
+# 如果需要手动安装, 以下是安装流程
+
+# ************************* Ubuntu *******************
+
+# apt-get install ca-certificates curl gnupg
+# install -m 0755 -d /etc/apt/keyrings
+# curl -fsSL https://gitee.com/songdaoyuan/hkzc-dev-ops-tools/raw/master/Docker/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# echo \
+#     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
+#     "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+#     tee /etc/apt/sources.list.d/docker.list >/dev/null
+# apt-get update
+# apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# ************************* Ubuntu *******************
+
+# ************************* CentOS *******************
+
+# yum install -y yum-utils
+# yum-config-manager --add-repo https://gitee.com/songdaoyuan/hkzc-dev-ops-tools/raw/master/Docker/centos/docker-ce.repo
+# sed -i 's+https://download.docker.com+https://mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
+# yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# ************************* CentOS *******************
