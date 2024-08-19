12a5e0fc3443120235f8eac832714d99f873b2eb
宋导 <835128023@qq.com>
2024-08-19 14:21:59 +0800

    update

--- shell/GetAvailableK8sNodePorts.sh
+++ shell/GetAvailableK8sNodePorts.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # 获取已使用的 K8s NodePort
-for port in {30000..32767}
-do
-  if ! kubectl get services --all-namespaces -o jsonpath='{.items[*].spec.ports[*].nodePort}' | grep -q "\b$port\b"; then
-    echo "Port $port is available"
-  fi
+used_ports=$(kubectl get services --all-namespaces -o jsonpath='{range .items[*]}{.spec.ports[*].nodePort}{"\n"}{end}' | sort -n | uniq)
+
+# 定义 NodePort 范围
+start_port=30000
+end_port=32767
+
+# 查找可用的 NodePort
+echo "Available NodePorts:"
+for ((port=start_port; port<=end_port; port++)); do
+    if ! echo "$used_ports" | grep -q "^$port$"; then
+        echo $port
+    fi
 done
