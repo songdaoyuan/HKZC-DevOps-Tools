2eb53404161b75b4fe1eaa73db74fa1619bfa543
宋导 <835128023@qq.com>
2024-08-20 16:45:58 +0800

    完成了Ubuntu/CentOS的静态网络配置函数

--- shell/OSInit.sh
+++ shell/OSInit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # code by songdaoyuan@20240816
 # 用于初始化部署后的Linux Server
-# 基础流程: 
+# 基础流程:
 #   1.配置镜像源、更新包列表和软件包、安装必备软件包
 #   2.关闭DHCP, 使用固定IP, 使用安全的DNS
 #   3.确保SSHD已经开启, 且启用了root的密码登录
@@ -38,6 +38,36 @@ config_date_sync() {
     chronyc -a makestep
 }
 
+get_netinfo(){
+    # 获取当前活动的网络接口
+    INTERFACE=$(ip route | grep default | awk '{print $5}')
+    # 获取当前IP地址
+    CURRENT_IP=$(ip addr show $INTERFACE | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+    # 获取默认网关
+    GATEWAY=$(ip route | grep default | awk '{print $3}')
+    # 获取子网掩码
+    NETMASK=$(ip addr show $INTERFACE | grep "inet\b" | awk '{print $2}' | cut -d/ -f2)
+
+    # 获取DNS服务器(主Ali, 副Cloudflare)
+    DNS1="223.5.5.5"
+    DNS2="1.1.1.1"
+
+    # 打印当前网络信息
+    echo "当前网络配置为:"
+    echo "活动的网卡: $INTERFACE"
+    echo "IP 地址: $CURRENT_IP"
+    echo "网关: $GATEWAY"
+    echo "子网掩码: $NETMASK"
+
+    # 询问是否要更改IP地址
+    read -p "需要修改当前的IP地址吗? (y/n): " change_ip
+
+    if [ "$change_ip" = "y" ]; then
+        read -p "输入新的IP地址: " NEW_IP
+    else
+        NEW_IP=$CURRENT_IP
+    fi
+}
 
 #*********************** 适用于 RH 系发行版的函数
 
@@ -47,10 +77,23 @@ RH_disable_selinux() {
 }
 
 RH_config_static_ip() {
-    ip addr show
+    get_netinfo
+    
+    # 创建新的网络配置文件
+    CONFIG_FILE="/etc/sysconfig/network-scripts/ifcfg-$INTERFACE"
 
+    echo "DEVICE=$INTERFACE" >$CONFIG_FILE
+    echo "BOOTPROTO=static" >>$CONFIG_FILE
+    echo "ONBOOT=yes" >>$CONFIG_FILE
+    echo "IPADDR=$NEW_IP" >>$CONFIG_FILE
+    echo "NETMASK=255.255.255.0" >>$CONFIG_FILE
+    echo "GATEWAY=$GATEWAY" >>$CONFIG_FILE
+    echo "DNS1=$DNS1" >>$CONFIG_FILE
+    echo "DNS2=$DNS2" >>$CONFIG_FILE
+
+    # 重启网络服务
+    systemctl restart network
 }
-
 
 #*********************** 适用于 Debain 系发行版的函数
 
@@ -58,6 +101,28 @@ disable_apparmor() {
     echo
 }
 
+DB_config_static_ip(){
+    get_netinfo
+    
+    # 创建新的Netplan配置文件
+    CONFIG_FILE="/etc/netplan/01-netcfg.yaml"
+
+    cat << EOF > $CONFIG_FILE
+    network:
+    version: 2
+    renderer: networkd
+    ethernets:
+        $INTERFACE:
+        dhcp4: no
+        addresses: [$NEW_IP/$NETMASK]
+        gateway4: $GATEWAY
+        nameservers:
+            addresses: [$DNS1, $DNS2]
+EOF
+
+    # 应用新的网络配置
+    netplan apply
+}
 
 config_firewall() {
     echo
@@ -72,8 +137,8 @@ config_firewall() {
 
 # 确保脚本以root权限运行
 if [ "$(id -u)" != "0" ]; then
-   echo "必须以root用户或者使用sudo权限来运行此脚本, 这个脚本会按序执行命令初始化SRV" 1>&2
-   exit 1
+    echo "必须以root用户或者使用sudo权限来运行此脚本, 这个脚本会按序执行命令初始化SRV" 1>&2
+    exit 1
 fi
 echo "在生产模式中启用root的SSH登录以及关闭SELinux / AppArmor是不安全的, 请注意做好服务器加固工作"
 
@@ -82,7 +147,8 @@ os_name=$(hostnamectl | grep 'Operating System' | awk '{print $3}')
 # 根据操作系统名称调用对应的包管理器
 case "$os_name" in
 "CentOS" | "Fedora" | "RHEL" | "Rocky" | "AlmaLinux")
-
+    
+    RH_config_static_ip
     # 配置镜像源、更新包列表和软件包、安装必备软件包
     yum update -y
     yum -y install curl vim openssh-server chrony
@@ -93,6 +159,7 @@ case "$os_name" in
     ;;
 "Ubuntu" | "Debian")
 
+    DB_config_static_ip
     # 配置镜像源、更新包列表和软件包、安装必备软件包
     apt update && apt upgrade -y
     apt install curl vim openssh-server chrony -y
