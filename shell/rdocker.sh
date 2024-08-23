b1a1b1a89d59bce6a104db0eeaa0df913bfff72c
宋导 <835128023@qq.com>
2024-08-23 17:43:33 +0800

    init & update rdocker

---
+++ shell/rdocker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 1.连接到镜像代理服务器MIRROR_SERVER -> 拉取镜像 -> 推送到harbor服务器 -> 从harbor服务器拉取到本地
+# code by songdaoyuan@20240801
+# update 20240823 修正了一些参数的命名方式
+
+# 使用方式
+# 首先将这个脚本放在如 /usr/local/bin 的系统变量目录, 使用 chmox +x 授予运行权限
+# rdocker $OPERATION $IMAGE_NAME_VERSION
+
+# ToDo 增加镜像存在性校验
+
+set -exu
+
+# 定义基本信息(镜像服务器 & harbor)
+MIRROR_SERVER=172.28.12.252
+# 修改为你自己的harbor服务器
+HARBOR_SERVER=harbor.domain.com/swap/
+
+OPERATION=$1 # 操作类型, 目前只支持 pull
+IMAGE_NAME_VERSION=$2 # 获取传入的镜像名和版本
+
+echo "第一次使用时, 确保你已经配置了${MIRROR_SERVER}的SSH免密登录"
+ssh root@$MIRROR_SERVER "docker pull ${IMAGE_NAME_VERSION}" && echo "remote docker pull successfully" || echo "remote docker pull failed"
+ssh root@$MIRROR_SERVER "docker tag ${IMAGE_NAME_VERSION} ${HARBOR_SERVER}${IMAGE_NAME_VERSION}"
+ssh root@$MIRROR_SERVER "docker push ${HARBOR_SERVER}${IMAGE_NAME_VERSION}"
+ssh root@$MIRROR_SERVER "docker rmi ${HARBOR_SERVER}${IMAGE_NAME_VERSION}"
+
+docker pull ${HARBOR_SERVER}${IMAGE_NAME_VERSION} && echo "local docker pull successfully" || echo "local docker pull failed"
