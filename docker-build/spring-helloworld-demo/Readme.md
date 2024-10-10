b064bb438e93402d31e4a0419d68ba60a8d602a8
宋导 <835128023@qq.com>
2024-10-10 14:54:56 +0800

    更新jre_8u422-spring-helloworld-demo配置文件

---
+++ docker-build/spring-helloworld-demo/Readme.md
@@ -0,0 +1,3 @@
+# 说明
+
+基于jre_8u422-dbg构建的spring-helloworld-demo, 访问 `http://容器IP:8080/index` 返回hello world
---
+++ docker-build/spring-helloworld-demo/build-spring-helloworld-demo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build --no-cache -f Dockerfile-spring-helloworld-demo -t jre_8u422-spring-helloworld-demo:v1 .
